e09e7e0adaeee81fcc96b448f17c89bdfa6e01cf
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-01-11 17:23:45 +0100

    api-clients/middleware-token-authorization

---
+++ src/TokenAuthorizationHeaderMiddleware.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Middleware\TokenAuthorization;
+
+use ApiClients\Foundation\Middleware\DefaultPriorityTrait;
+use ApiClients\Foundation\Middleware\MiddlewareInterface;
+use ApiClients\Foundation\Middleware\PostTrait;
+use Psr\Http\Message\RequestInterface;
+use React\Promise\CancellablePromiseInterface;
+use function React\Promise\resolve;
+
+/**
+ * Middleware that adds the authorization header in the token format.
+ */
+class TokenAuthorizationHeaderMiddleware implements MiddlewareInterface
+{
+    use DefaultPriorityTrait;
+    use PostTrait;
+
+    /**
+     * @param RequestInterface $request
+     * @param array $options
+     * @return CancellablePromiseInterface
+     */
+    public function pre(RequestInterface $request, array $options = []): CancellablePromiseInterface
+    {
+        if (!isset($options[self::class][Options::TOKEN])) {
+            return resolve($request);
+        }
+
+        if (empty($options[self::class][Options::TOKEN])) {
+            return resolve($request);
+        }
+
+        return resolve(
+            $request->withAddedHeader(
+                'Authorization',
+                'token ' . $options[self::class][Options::TOKEN]
+            )
+        );
+    }
+}
---
+++ tests/TokenAuthorizationHeaderMiddlewareTest.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Middleware\TokenAuthorization;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use React\EventLoop\Factory;
+use RingCentral\Psr7\Request;
+use ApiClients\Middleware\TokenAuthorization\TokenAuthorizationHeaderMiddleware;
+use ApiClients\Middleware\TokenAuthorization\Options;
+use function Clue\React\Block\await;
+use function React\Promise\resolve;
+
+final class TokenAuthorizationHeaderMiddlewareTest extends TestCase
+{
+    public function preProvider()
+    {
+        yield [
+            [],
+            false,
+            ''
+        ];
+
+        yield [
+            [
+                TokenAuthorizationHeaderMiddleware::class => [
+                    Options::TOKEN => '',
+                ],
+            ],
+            false,
+            ''
+        ];
+
+        yield [
+            [
+                TokenAuthorizationHeaderMiddleware::class => [
+                    Options::TOKEN => null,
+                ],
+            ],
+            false,
+            ''
+        ];
+
+        yield [
+            [
+                TokenAuthorizationHeaderMiddleware::class => [
+                    Options::TOKEN => 'kroket',
+                ],
+            ],
+            true,
+            'token kroket'
+        ];
+    }
+
+    /**
+     * @dataProvider preProvider
+     */
+    public function testPre(array $options, bool $hasHeader, string $expectedHeader)
+    {
+        $request = new Request('GET', 'https://example.com/');
+        $middleware = new TokenAuthorizationHeaderMiddleware();
+        $changedRequest = await($middleware->pre($request, $options), Factory::create());
+
+        if ($hasHeader === false) {
+            self::assertFalse($changedRequest->hasHeader('Authorization'));
+            return;
+        }
+
+        self::assertTrue($changedRequest->hasHeader('Authorization'));
+        self::assertSame($expectedHeader, $changedRequest->getHeaderLine('Authorization'));
+    }
+}
