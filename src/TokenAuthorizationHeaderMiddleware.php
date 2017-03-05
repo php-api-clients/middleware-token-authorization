0b3f4ecbaf088cd21cb586b2e09d2e8c9675e3b0
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-05 22:03:38 +0100

    Updated middleware to 2.0

--- src/TokenAuthorizationHeaderMiddleware.php
+++ src/TokenAuthorizationHeaderMiddleware.php
@@ -3,6 +3,7 @@
 namespace ApiClients\Middleware\TokenAuthorization;
 
 use ApiClients\Foundation\Middleware\DefaultPriorityTrait;
+use ApiClients\Foundation\Middleware\ErrorTrait;
 use ApiClients\Foundation\Middleware\MiddlewareInterface;
 use ApiClients\Foundation\Middleware\PostTrait;
 use Psr\Http\Message\RequestInterface;
@@ -16,6 +17,7 @@ class TokenAuthorizationHeaderMiddleware implements MiddlewareInterface
 {
     use DefaultPriorityTrait;
     use PostTrait;
+    use ErrorTrait;
 
     /**
      * @param RequestInterface $request
